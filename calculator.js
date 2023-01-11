f25af3ad47571b329f1b17f93a5e86cdd14b287a
vipsmmtime <vipsmmtime@yandex.ru>
2023-01-11 23:38:20 +0300

    js

---
+++ calculator.js
@@ -0,0 +1,33 @@
+const OPERATION = {
+    sum: '+',
+    substract: '-',
+    multiply: '*',
+    divison: '/'
+}
+
+function calculate({a, b, operation}) {
+    let result = null;
+
+    switch (operation) {
+        case OPERATION.sum:
+            result = sum(a, b);
+            break;
+
+        case OPERATION.substract:
+            result = substract(a, b);
+            break;
+
+        case OPERATION.multiply:
+            result = multiply(a, b);
+            break;
+
+        case OPERATION.divison:
+            result = divison(a, b);
+            break;
+
+        default:
+            break;
+    }
+
+    return result;
+}
